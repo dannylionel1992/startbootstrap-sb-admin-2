c93adfd727f465276d238c46211546ab0c763176
dannylionel1992 <52813065+dannylionel1992@users.noreply.github.com>
2020-07-22 17:03:25 +0200

    June/July
    Changed month from june to july on bar graph

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["January", "February", "March", "April", "May", "July"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
